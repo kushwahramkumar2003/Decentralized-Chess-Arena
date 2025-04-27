5680481d70aac20e249f70d9a12f500c352f7a7f
kushwahramkumar2003 <kushwahramkumar2003@gmail.com>
2025-04-27 11:30:06 +0530

    build error resolved

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["src/generated/**"], // Ignore all files in src/generated
+  },
 ];
 
 export default eslintConfig;
